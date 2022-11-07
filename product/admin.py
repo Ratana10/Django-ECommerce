816d5aed98d7ec5eeff9ec592cedfed0d4882ee9
Ratana10 <sanratana18@gmail.com>
2022-11-08 00:22:52 +0700

    My first commit

---
+++ product/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent', 'status']
+    list_filter = ['status']
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status']
+    list_filter = ['category']
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
---
+++ product/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    
+    parent = models.ForeignKey('self', blank=True, null=True, related_name = 'children', on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keyword = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+  
+    
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    #one to many  
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)# one product have one category -> one category have many product
+    title = models.CharField(max_length=30)
+    keyword = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    amount = models.IntegerField()
+    minamount = models.IntegerField()
+    detail = models.TextField()
+    slug = models.SlugField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
