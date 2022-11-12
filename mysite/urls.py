eaebc5445e8a1cbb1f999835ec47aa241b90adbd
Ratana10 <sanratana18@gmail.com>
2022-11-12 12:18:50 +0700

    login logout userprofile

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 from order import views  as orderViews
+from user import views  as userViews
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -35,6 +36,9 @@ urlpatterns = [
     path('searchAuto/', views.searchAuto, name='searchAuto'),
     path('product/<int:id>/<slug:slug>', views.productDetail, name='productDetail'),
     path('shopcart/', orderViews.shopCart, name='shopCart'),
+    path('login/', userViews.loginForm, name='login'),
+    path('logout/', userViews.logoutFun, name='logout'),
+    path('singup/', userViews.signunForm, name='signup'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- user/views.py
+++ user/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from product.models import Category
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
+
+from home.models import Setting, ContactForm, ContactMessage
+
+@login_required(login_url='/login') # Check login
 def index(request):
     return HttpResponse('User')
+
+def loginForm(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user )
+            current_user = request.user
+            # print(f'testing {current_user.id}')
+            userprofile = UserProfile.objects.get(user_id=current_user.id)
+            print(f'testing {userprofile.image.url}')
+            request.session['userimage'] = userprofile.image.url
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, 'Login Failed  ! Username or Password incorrect')
+    category = Category.objects.all()
+    context = {
+       'category' : category
+    }
+    return render(request, 'loginForm.html', context)
+
+def signunForm(request):
+    category = Category.objects.all()
+    context = {
+        'category': category,
+    }
+    return render(request, 'loginForm.html', context)
+
+def logoutFun(request):
+    logout(request)
+    return HttpResponseRedirect('/')
